975632fc2908c5703c1b918cdc4340151ef212c0
twipzy <100216841+twipzy@users.noreply.github.com>
2023-06-05 01:48:25 +0200

    idk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import * as React from "react"
+// import { Radio, Alert, Button, Card, Progress, Result } from "antd"
+import "../assets/style.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="container">
+    <div className="header">
+    </div>
+    <ul className="menu">
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Driving Tests</a>
+      </li>
+      <li>
+        <a href="#">Personalised Licence Plates</a>
+      </li>
+      <li className="exit"><a href="#" role="buton">Exit</a></li>
+    </ul>
+    </div>
+  )
 }
