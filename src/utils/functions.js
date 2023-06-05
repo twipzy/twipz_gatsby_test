6832250e07c86eef62a42e45ef72cca794eb6969
twipzy <100216841+twipzy@users.noreply.github.com>
2023-06-05 13:52:21 +0200

    add: functions.js

---
+++ src/utils/functions.js
@@ -0,0 +1,7 @@
+export function exit() {
+    fetch('http://highlife/CloseDMVNui', {
+        method: "POST",
+        headers: { "Content-Type": "application/json; charset=UTF-8" },
+        body: JSON.stringify({}),
+    }).then(resp => resp.json())
+}
